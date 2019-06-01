9b939484a8a56eb66658b78d8ff20a02566f6c25
Zaicev Vladimir <zaicevvv1991@gmail.com>
2019-06-01 11:33:56 +0300

    Updates

---
+++ precache-manifest.e45f8b61383cbd899077d0e7004f6db7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fba6cca3d98239d409bd501f99ae823",
+    "url": "/goit-react-hw-03-book-search/index.html"
+  },
+  {
+    "revision": "6f58d348db8224bf4707",
+    "url": "/goit-react-hw-03-book-search/static/js/2.6006a796.chunk.js"
+  },
+  {
+    "revision": "63f71d3a3465e12cf2b4",
+    "url": "/goit-react-hw-03-book-search/static/js/main.0a9f5a56.chunk.js"
+  },
+  {
+    "revision": "94506de2c381820c5c24",
+    "url": "/goit-react-hw-03-book-search/static/js/runtime~main.5d5bdee0.js"
+  }
+]);
